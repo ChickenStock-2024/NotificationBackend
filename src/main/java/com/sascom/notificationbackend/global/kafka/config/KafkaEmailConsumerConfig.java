2a233295f511e178d9b7380af8b400f454f530a9
given-dragon <yijy001@gmail.com>
2024-08-08 23:10:23 +0900

    [Fix] #3 - fmcConfig 생성 대비 메서드 명 수정 및 에러 핸들링 추가

--- src/main/java/com/sascom/notificationbackend/global/kafka/config/KafkaEmailConsumerConfig.java
+++ src/main/java/com/sascom/notificationbackend/global/kafka/config/KafkaEmailConsumerConfig.java
@@ -4,32 +4,39 @@ import com.sascom.notificationbackend.global.dto.NotificationMessageDto;
 import com.sascom.notificationbackend.global.kafka.properties.EmailConsumerProperties;
 import com.sascom.notificationbackend.global.kafka.properties.FcmConsumerProperties;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.util.backoff.BackOff;
+import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
+@Slf4j
 @EnableKafka
 @Configuration
 @RequiredArgsConstructor
 public class KafkaEmailConsumerConfig {
 
     private final EmailConsumerProperties emailConsumerProperties;
-    private final FcmConsumerProperties fcmConsumerProperties;
+
+    @Value(value = "${kafka.backoff.interval}")
+    private Long interval;
+    @Value(value = "${kafka.backoff.max_failure}")
+    private Long maxAttempts;
 
     @Bean
-    public Map<String, Object> consumerConfig() {
+    public Map<String, Object> emailConsumerConfig() {
 
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
@@ -48,20 +55,28 @@ public class KafkaEmailConsumerConfig {
     }
 
     @Bean
-    public ConsumerFactory<String, NotificationMessageDto> consumerFactory() {
+    public ConsumerFactory<String, NotificationMessageDto> emailConsumerFactory() {
 
         StringDeserializer stringDeserializer = new StringDeserializer();
         JsonDeserializer<NotificationMessageDto> jsonDeserializer =
                 new JsonDeserializer(NotificationMessageDto.class, false);
 
-        return new DefaultKafkaConsumerFactory<>(consumerConfig(), stringDeserializer, jsonDeserializer);
+        return new DefaultKafkaConsumerFactory<>(emailConsumerConfig(), stringDeserializer, jsonDeserializer);
     }
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, NotificationMessageDto> kafkaListenerContainerFactory() {
+    public ConcurrentKafkaListenerContainerFactory<String, NotificationMessageDto> emailKafkaListenerContainerFactory() {
         ConcurrentKafkaListenerContainerFactory<String,NotificationMessageDto> kafkaListenerContainerFactory
                 = new ConcurrentKafkaListenerContainerFactory<>();
-        kafkaListenerContainerFactory.setConsumerFactory(consumerFactory());
+        kafkaListenerContainerFactory.setConsumerFactory(emailConsumerFactory());
         return kafkaListenerContainerFactory;
+    }
+
+    @Bean
+    public DefaultErrorHandler emailErrorHandler() {
+        BackOff fixedBackOff = new FixedBackOff(interval, maxAttempts);
+        return new DefaultErrorHandler((consumerRecord, e) ->
+                log.error("consumed record: {}", consumerRecord), fixedBackOff
+        );
     }
 }

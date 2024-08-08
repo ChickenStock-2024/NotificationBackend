f091531fc0130312715f3bb53108a54bb55f62eb
given-dragon <yijy001@gmail.com>
2024-08-08 23:15:24 +0900

    [Feat] #3 - fcm consumer 구현

--- src/main/java/com/sascom/notificationbackend/firebase/controller/FirebaseController.java
+++ src/main/java/com/sascom/notificationbackend/firebase/controller/FirebaseController.java
@@ -2,6 +2,7 @@ package com.sascom.notificationbackend.firebase.controller;
 
 import com.sascom.notificationbackend.firebase.application.FirebaseMessageSender;
 import com.sascom.notificationbackend.global.dto.NotificationMessageDto;
+import com.sascom.notificationbackend.global.kafka.producer.KafkaProducer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,20 +12,12 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class FirebaseController {
     private final FirebaseMessageSender messageSender;
+    private final KafkaProducer kafkaProducer;
 
     @PostMapping
-    public ResponseEntity<?> messagingTest(@RequestBody NotificationMessageDto requestDto) {
-        messageSender.sendMessage(requestDto.receiver(), requestDto.title(), requestDto.body());
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping
-    public ResponseEntity<?> messageFront() {
-        messageSender.sendMessage(
-                "fdmWMBXKYaI446SYV27ZiB:APA91bHHSNpPhsnX89eHMRARsG6587lnlwUZtOccQpqZfSRXL58zmX6ar5f1LTfeovQIHurERRifMJHX-wlp5f6Orr2Km-RSw7OxxdcnUphOrKZUiegjqgnGUz64Up3rLhQkoVjp1a3x",
-                "알림 타이틀22222",
-                "축하드립니다 :)"
-        );
+    public ResponseEntity<?> messaging(@RequestBody NotificationMessageDto requestDto) {
+//        messageSender.sendMessage(requestDto.receiver(), requestDto.title(), requestDto.body());
+        kafkaProducer.sendAlarmMessageToKafka(requestDto);
         return ResponseEntity.ok().build();
     }
 }

e99246c1f1dd13b53b9c60a28b37206bf67699ce
given_dragon <yijy001@naver.com>
2024-08-08 17:53:44 +0900

    [Add] #3 - kafka properties 객체 생성

--- src/main/java/com/sascom/notificationbackend/NotificationBackendApplication.java
+++ src/main/java/com/sascom/notificationbackend/NotificationBackendApplication.java
@@ -2,7 +2,9 @@ package com.sascom.notificationbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class NotificationBackendApplication {
 

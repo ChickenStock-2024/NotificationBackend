61070c78c1c045f9086d56be82d2fa04e8efda1f
given-dragon <yijy001@gmail.com>
2024-08-08 23:17:46 +0900

    [Refactor] #3 - email로 이름 변경

--- src/main/java/com/sascom/notificationbackend/global/kafka/consumer/EmailConsumer.java
+++ src/main/java/com/sascom/notificationbackend/global/kafka/consumer/EmailConsumer.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class EmailMessageConsumer {
+public class EmailConsumer {
 
     private final EmailSender emailSender;
 

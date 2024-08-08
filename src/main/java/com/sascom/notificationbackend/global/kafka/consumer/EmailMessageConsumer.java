2bf13c8c3849dc2370ee681ec3599a8a8d394525
given-dragon <yijy001@gmail.com>
2024-08-08 23:12:48 +0900

    [Add] #3 - EmailConsumer concurrency 설정 추가, 실행시간 로깅

--- src/main/java/com/sascom/notificationbackend/global/kafka/consumer/EmailMessageConsumer.java
+++ src/main/java/com/sascom/notificationbackend/global/kafka/consumer/EmailMessageConsumer.java
@@ -8,8 +8,6 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -17,15 +15,19 @@ public class EmailMessageConsumer {
 
     private final EmailSender emailSender;
 
-    @KafkaListener(topics = "email", groupId = "email_consumer", containerFactory = "kafkaListenerContainerFactory")
+    @KafkaListener(
+            topics = "${kafka.email-consumer.default-topic}",
+            groupId = "${kafka.email-consumer.group-id}",
+            containerFactory = "emailKafkaListenerContainerFactory",
+            concurrency = "3"
+    )
     public void consumeEmailMessage(ConsumerRecord<String, NotificationMessageDto> messageDto) {
         long startTime = System.nanoTime();
 
-        System.out.println("Success to reach Sender, Topic = Email");
-        System.out.printf("messageDto = %s\n", messageDto);
+        log.info("Success to reach Sender, Topic = Email");
         NotificationMessageDto nmd = messageDto.value();
         emailSender.sendMessage(nmd.receiver(), nmd.title(), nmd.body());
-        System.out.printf("Consumed Received Email Message : %s%n", nmd);
+        log.info("Consumed Received Email Message : {}}", nmd);
 
         // 종료 시간 기록
         long endTime = System.nanoTime();
@@ -33,8 +35,8 @@ public class EmailMessageConsumer {
         // 실행 시간 계산 (나노초 단위)
         long duration = endTime - startTime;
 
-        // 실행 시간 출력 (밀리초 단위로 변환)
-        System.out.println("Execution time: " + (duration) + " nanoseconds");
+        // 실행 시간 출력 (초 단위로 변환)
+        double executionTimeSec = (double) duration / 1000000000;
+        log.info("Execution time: {} seconds", executionTimeSec);
     }
-
 }
